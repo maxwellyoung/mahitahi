95e294f490f59ce44059f9afc7527c102ef00c2c
Maxwell Young <maxtheyoung@gmail.com>
2024-08-27 19:13:32 +1200

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value:
+              "private-state-token-redemption=(), private-state-token-issuance=(), browsing-topics=()",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+  },
+  experimental: {
+    optimizeFonts: true,
+  },
+};
 
 export default nextConfig;
